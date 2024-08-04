4f2df5d5b8f2907905fab28cd7b5c5057fbc3967
Varan Mehra <varan@Varans-MacBook-Pro.local>
2024-08-04 03:38:18 -0500

    Address validator PR comments

--- src/elitefurretai/utils/team_repo.py
+++ src/elitefurretai/utils/team_repo.py
@@ -6,73 +6,110 @@
 import os
 import os.path
 import subprocess
-from typing import Dict
+from contextlib import contextmanager
+from typing import Dict, KeysView
 
 
 # TODO: can convert them into json and add team parsing/format information, with properties?
 class TeamRepo:
 
-    def __init__(self, filepath: str = "data/teams"):
-        self._teams: Dict[str, str] = {}
+    def __init__(
+        self,
+        filepath: str = "data/teams",
+        verbose: bool = False,
+        showdown_path: str = "../pokemon-showdown",
+    ):
+        self._teams: Dict[str, Dict[str, str]] = {}
 
         for format_folder in os.listdir(filepath):
             format_name = format_folder
             folder_path = os.path.join(filepath, format_folder)
+
             if os.path.isdir(folder_path):
-                print(f"format name: {format_name}")
+                if verbose:
+                    print(f"format: {format_name}")
+                if format not in self._teams:
+                    self._teams[format_name] = {}
+
                 filenames = os.listdir(folder_path)
                 for filename in filenames:
                     team_path = os.path.join(filepath, format_folder, filename)
-                    print(team_path)
+                    if verbose:
+                        print(team_path)
                     with open(team_path, "r") as f:
                         team_string = f.read()
-                    self._teams[
-                        format_name + "_" + os.path.basename(team_path).replace(".txt", "")
-                    ] = team_string
-                    self.validate_team(team_string, format_name)
+                        self._teams[format_name][
+                            os.path.basename(team_path).replace(".txt", "")
+                        ] = team_string
+                    self.validate_team(team_string, format_name, verbose, showdown_path)
+                print(format_name)
 
-    # Returns True if valid
+    @staticmethod
+    @contextmanager
+    def change_dir(path):
+        origin = os.getcwd()
+        try:
+            os.chdir(path)
+            yield
+        finally:
+            os.chdir(origin)
+
     def validate_team(
-        self, team: str, format: str, SHOWDOWN_PATH: str = "../pokemon-showdown"
+        self,
+        team: str,
+        format: str,
+        verbose: bool = False,
+        showdown_path: str = "../pokemon-showdown",
     ) -> bool:
 
-        output = True
-        ELITEFURRETAI_PATH = os.getcwd()
-        os.chdir(SHOWDOWN_PATH)
+        SHOWDOWN_PATH = os.path.abspath(os.path.expanduser(showdown_path))
+        with self.change_dir(SHOWDOWN_PATH):
 
-        try:
-            result = subprocess.run(
-                ["./pokemon-showdown", "pack-team"],
-                input=team,
-                text=True,
-                capture_output=True,
-                check=True,
-            )
-            packed_team = result.stdout.strip()
-        except subprocess.CalledProcessError as e:
-            print(f"Error packing team: {e}")
-            print(f"Error output: {e.stderr}")
-            output = False
+            try:
+                result = subprocess.run(
+                    ["./pokemon-showdown", "pack-team"],
+                    input=team,
+                    text=True,
+                    capture_output=True,
+                    check=True,
+                )
+                packed_team = result.stdout.strip()
+            except subprocess.CalledProcessError as e:
+                if verbose:
+                    print(f"Error packing team: {e}")
+                    print(f"Error output: {e.stderr}")
+                return False
 
-        try:
-            result = subprocess.run(
-                ["./pokemon-showdown", "validate-team", format],
-                input=packed_team,
-                text=True,
-                capture_output=True,
-                check=True,
-            )
-            print(result.stdout)
-        except subprocess.CalledProcessError as e:
-            print(f"Error validating team: {e}")
-            print(f"Format: {format}")
-            print(f"Team (packed): {packed_team}")
-            print(f"Error output: {e.stderr}")
-            output = False
+            try:
+                result = subprocess.run(
+                    ["./pokemon-showdown", "validate-team", format],
+                    input=packed_team,
+                    text=True,
+                    capture_output=True,
+                    check=True,
+                )
+                if verbose:
+                    print(result.stdout)
+            except subprocess.CalledProcessError as e:
+                if verbose:
+                    print(f"Error validating team: {e}")
+                    print(f"Format: {format}")
+                    print(f"Team (packed): {packed_team}")
+                    print(f"Error output: {e.stderr}")
+                return False
 
-        os.chdir(ELITEFURRETAI_PATH)
-        return output
+        return True
 
     @property
-    def teams(self) -> Dict[str, str]:
+    def teams(self) -> Dict[str, Dict[str, str]]:
         return self._teams
+
+    @property
+    def formats(self) -> KeysView[str]:
+        return self._teams.keys()
+
+    def get(self, format="", name="") -> str:
+        return self._teams[format][name]
+
+    def get_all(self, format="") -> Dict[str, str]:
+        return self._teams[format]
--- unit_tests/utils/test_team_repo.py
+++ unit_tests/utils/test_team_repo.py
@@ -1,16 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 
+import pytest
+
 from elitefurretai.utils.team_repo import TeamRepo
 
 
-def test_team_repo():
-    tr = TeamRepo("data/teams")
-    assert "gen9vgc2024regg_speed_ability1" in tr.teams
-    assert "gen8vgc2020_s6_yan" in tr.teams
-    assert len(tr.teams) > 5
-    for team in tr.teams:
-        assert isinstance(tr.teams[team], str)
+@pytest.fixture
+def tr():
+    return TeamRepo("data/teams")
 
 
 # helper function
@@ -19,9 +17,47 @@ def get_paste(filepath: str) -> str:
         return f.read()
 
 
-def test_validator():
+def test_team_repo(tr):
+    # Test 'teams' getter
+    assert "speed_ability1" in tr.teams["gen9vgc2024regg"]
+    assert "s6_yan" in tr.teams["gen8vgc2020"]
+    assert "s2_koutesh" in tr.teams["gen8vgc2020"]
+    assert "worlds_rattle" in tr.teams["gen8vgc2022"]
+    print(tr.teams["gen8vgc2020"].keys())
+    assert len(tr.teams["gen8vgc2022"]) > 3
+    for team in tr.teams["gen8vgc2020"]:
+        assert isinstance(tr.teams["gen8vgc2020"][team], str)
+
+    # Test formats getter
+    assert len(tr.formats) >= 5
+    assert "gen8vgc2021" in tr.formats
+    assert "gen9vgc2023regd" in tr.formats
+    assert "invalidformat" not in tr.formats
+
+    # Test 'get()'
+    assert (
+        tr.get(format="gen9vgc2023regd", name="worlds_kelsch")
+        == tr.teams["gen9vgc2023regd"]["worlds_kelsch"]
+    )
+    assert (
+        tr.get(format="gen8vgc2021", name="s7_zheng")
+        == tr.teams["gen8vgc2021"]["s7_zheng"]
+    )
+    assert isinstance(tr.get("gen9vgc2023regd", "worlds_nielsen"), str)
+    with pytest.raises(KeyError):
+        tr.get(format="invalidformat", name="invalidteam")
+        tr.get(format="gen9vgc2023regd", name="invalidteam")
+
+    # Test 'get_all()'
+    assert tr.get_all(format="gen8vgc2020") == tr.teams["gen8vgc2020"]
+    assert len(tr.get_all(format="gen8vgc2022")) == len(tr.teams["gen8vgc2022"])
+    assert isinstance(tr.get_all("gen8vgc2021"), dict)
+    with pytest.raises(KeyError):
+        tr.get_all(format="invalidformat")
+
+
+def test_validator(tr):
     teams_folder = "data/teams"
-    tr = TeamRepo(teams_folder)
     formats = os.listdir(teams_folder)
 
     # test invalidity for all formats
@@ -36,11 +72,11 @@ def test_validator():
 
     # test validity for all formats
     format_name = "gen8vgc2020"
-    assert tr.validate_team(tr.teams["gen8vgc2020_s2_koutesh"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2020_s3_eakes"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2020_s4_morgan"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2020_s5_tarquinio"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2020_s6_yan"], format_name) is True
+    assert tr.validate_team(tr.teams["gen8vgc2020"]["s2_koutesh"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2020"]["s3_eakes"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2020"]["s4_morgan"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2020"]["s5_tarquinio"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2020"]["s6_yan"], format_name)
 
     # gen8vgc2021 uses series 7/9 rules, so 8/10 should be invalid!
     format_name = "gen8vgc2021"
@@ -48,48 +84,45 @@ def test_validator():
     s10_team = get_paste("data/fixture/test_teams/s10_patel.txt")
     assert tr.validate_team(s8_team, format_name) is False
     assert tr.validate_team(s10_team, format_name) is False
-    assert tr.validate_team(tr.teams["gen8vgc2021_s7_zheng"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2021_s9_silva"], format_name) is True
+    assert tr.validate_team(tr.teams["gen8vgc2021"]["s7_zheng"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2021"]["s9_silva"], format_name)
 
     format_name = "gen8vgc2022"
-    assert tr.validate_team(tr.teams["gen8vgc2022_s11_clover"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2022_s12_mott"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2022_worlds_baek"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2022_worlds_chua"], format_name) is True
-    assert tr.validate_team(tr.teams["gen8vgc2022_worlds_rattle"], format_name) is True
+    assert tr.validate_team(tr.teams["gen8vgc2022"]["s11_clover"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2022"]["s12_mott"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2022"]["worlds_baek"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2022"]["worlds_chua"], format_name)
+    assert tr.validate_team(tr.teams["gen8vgc2022"]["worlds_rattle"], format_name)
 
     format_name = "gen9vgc2023regd"
-    assert tr.validate_team(tr.teams["gen9vgc2023regd_worlds_kelsch"], format_name) is True
-    assert (
-        tr.validate_team(tr.teams["gen9vgc2023regd_worlds_nielsen"], format_name) is True
-    )
+    assert tr.validate_team(tr.teams["gen9vgc2023regd"]["worlds_kelsch"], format_name)
+    assert tr.validate_team(tr.teams["gen9vgc2023regd"]["worlds_nielsen"], format_name)
 
     format_name = "gen9vgc2024regg"
-    assert tr.validate_team(tr.teams["gen9vgc2024regg_p1"], format_name) is True
-    assert tr.validate_team(tr.teams["gen9vgc2024regg_p2"], format_name) is True
-    assert (
-        tr.validate_team(tr.teams["gen9vgc2024regg_residual_speed1"], format_name) is True
-    )
-    assert (
-        tr.validate_team(tr.teams["gen9vgc2024regg_residual_speed2"], format_name) is True
-    )
-    assert (
-        tr.validate_team(tr.teams["gen9vgc2024regg_speed_ability1"], format_name) is True
-    )
-    assert tr.validate_team(tr.teams["gen9vgc2024regg_miraidon_bal"], format_name) is True
+    # These two teams should be flagged as invalid
+    assert tr.validate_team(tr.teams["gen9vgc2024regg"]["p1"], format_name) is False
+    assert tr.validate_team(tr.teams["gen9vgc2024regg"]["p2"], format_name) is False
+    assert tr.validate_team(tr.teams["gen9vgc2024regg"]["residual_speed1"], format_name)
+    assert tr.validate_team(tr.teams["gen9vgc2024regg"]["residual_speed2"], format_name)
+    assert tr.validate_team(tr.teams["gen9vgc2024regg"]["speed_ability1"], format_name)
+    assert tr.validate_team(tr.teams["gen9vgc2024regg"]["miraidon_bal"], format_name)
 
-    # Use wrong formats to test we aren't getting flase positives
+    # Use wrong formats to test we aren't getting false positives
     reg_e_team = get_paste("data/fixture/test_teams/reg_e_team.txt")
     assert tr.validate_team(reg_e_team, "gen9vgc2023regd") is False
     assert (
-        tr.validate_team(tr.teams["gen9vgc2024regg_miraidon_bal"], "gen9vgc2023regd")
+        tr.validate_team(tr.teams["gen9vgc2024regg"]["miraidon_bal"], "gen9vgc2023regd")
         is False
     )
     assert (
-        tr.validate_team(tr.teams["gen9vgc2024regg_miraidon_bal"], "gen8vgc2022") is False
+        tr.validate_team(tr.teams["gen9vgc2024regg"]["miraidon_bal"], "gen8vgc2022")
+        is False
     )
     assert (
-        tr.validate_team(tr.teams["gen8vgc2022_worlds_baek"], "gen9vgc2024regg") is False
+        tr.validate_team(tr.teams["gen8vgc2022"]["worlds_baek"], "gen9vgc2024regg")
+        is False
     )
-    assert tr.validate_team(tr.teams["gen8vgc2022_worlds_chua"], "gen8vgc2021") is False
-    assert tr.validate_team(tr.teams["gen8vgc2022_worlds_rattle"], "gen8vgc2020") is False
+    assert tr.validate_team(tr.teams["gen8vgc2022"]["worlds_chua"], "gen8vgc2021") is False
+    assert (
+        tr.validate_team(tr.teams["gen8vgc2022"]["worlds_rattle"], "gen8vgc2020") is False
+    )
